0808e450d6608cbcad3ec28f0bf7838ff32f7bb5
Your Name <example@example.com>
2026-06-07 13:18:45 +0200

    fix: add missing whitespace around operator

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -144,4 +144,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CRISPY_TEMPLATE_PACK="bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
